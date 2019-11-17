2b3f4e16095c2c5981bcccdda925f9b5faa83fb7
Lukáš Rác <lukiss482@gmail.com>
2019-11-17 15:01:29 +0100

    Added smooth scroll functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,12 @@
 import { Navigation } from './ui/scroll.js';
 
+// Add active class to nav on scroll
 const nav = document.querySelector('.navbar');
 
 const stickyNav = new Navigation(nav);
 stickyNav.sticky();
+
+// Set smooth scroll
+var scroll = new SmoothScroll('a[href*="#"]', {
+  updateURL: false,
+});
