c31f5276aa5db185ac04c4466ba4dc2daf960c29
Lukáš Rác <lukiss482@gmail.com>
2019-11-23 17:34:28 +0100

    Reorganized files

--- src/index.js
+++ src/index.js
@@ -5,8 +5,3 @@ const nav = document.querySelector('.navbar');
 
 const stickyNav = new Navigation(nav);
 stickyNav.sticky();
-
-// Set smooth scroll
-var scroll = new SmoothScroll('a[href*="#"]', {
-  updateURL: false,
-});
