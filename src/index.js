c610e3e559a028d6aaffba97312827dd77312e64
Lukáš Rác <lukiss482@gmail.com>
2019-11-23 19:06:57 +0100

    Added back-to-top button and fixed header section on IE

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import { Navigation } from './ui/scroll.js';
+import SmoothScroll from 'smooth-scroll';
+import { StickyNavigation, BackToTop } from './ui/scroll.js';
+
+// Activate smooth scrolling
+const scroll = new SmoothScroll('a[href*="#"]', {
+  updateURL: false,
+});
 
 // Add active class to nav on scroll
 const nav = document.querySelector('.navbar');
 
-const stickyNav = new Navigation(nav);
+const stickyNav = new StickyNavigation(nav);
 stickyNav.sticky();
+
+// Showing/hiding back to top button
+const link = document.querySelector('.back-to-top');
+
+const backToTop = new BackToTop(link);
+backToTop.init();
--- src/ui/scroll.js
+++ src/ui/scroll.js
@@ -1,4 +1,4 @@
-export class Navigation {
+export class StickyNavigation {
   constructor(container) {
     this.container = container;
   }
@@ -10,6 +10,28 @@ export class Navigation {
         this.container.classList.add('sticky');
       } else {
         this.container.classList.remove('sticky');
+      }
+    });
+  }
+}
+
+export class BackToTop {
+  constructor(link) {
+    this.link = link;
+  }
+  init() {
+    window.addEventListener('scroll', () => {
+      if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+      ) {
+        this.link.style.opacity = '1';
+        this.link.style.display = 'block';
+      } else {
+        this.link.style.opacity = '0';
+        this.link.addEventListener('transitionend', () => {
+          this.link.style.display = 'none';
+        });
       }
     });
   }
