e45a742d9012a9ff29b24a4234eae750b364086d
Lukáš Rác <lukiss482@gmail.com>
2020-01-27 00:19:43 +0100

    Work on project section continued

--- src/index.js
+++ src/index.js
@@ -17,3 +17,27 @@ const link = document.querySelector('.back-to-top');
 
 const backToTop = new BackToTop(link);
 backToTop.init();
+
+// isotope
+const grid = document.querySelector('.projects__container');
+const iso = new Isotope(grid, {
+  itemSelector: '.project__item',
+  layoutMode: 'masonry',
+});
+
+const filters = document.querySelectorAll('.projects__filter-item');
+const buttons = document.querySelectorAll('.projects__filter-category');
+
+buttons.forEach(button => {
+  button.addEventListener('click', e => {
+    const thisItem = e.target.parentElement;
+    filters.forEach(filter => {
+      filter.classList.remove('active');
+      if (thisItem === filter) {
+        thisItem.classList.toggle('active');
+      }
+    });
+    let filterValue = e.target.getAttribute('data-filter');
+    iso.arrange({ filter: filterValue });
+  });
+});
