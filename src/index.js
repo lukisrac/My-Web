dd295e2dbdb3c8ed5d0b285170c423ce7b2aeaba
Lukáš Rác <lukis4822@seznam.cz>
2020-03-26 22:37:48 +0100

    Some changes on form validation

--- src/index.js
+++ src/index.js
@@ -42,7 +42,11 @@ const phoneInput = contactForm.querySelector('#phone');
 const serviceTypeInput = contactForm.querySelector('#service-type');
 const messageInput = contactForm.querySelector('#message');
 
-let isValidName, isValidEmail, isValidPhone, isValidServiceType, isValidMessage;
+let isValidName,
+  isValidEmail,
+  isValidPhone,
+  isValidServiceType,
+  isValidMessage = true;
 
 const validateEmail = email => {
   let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -50,37 +54,31 @@ const validateEmail = email => {
   return regex.test(email);
 };
 
-const validateOnBlur = () => {
-  nameInput.addEventListener('blur', () => {
-    if (nameInput.value !== '') {
-      nameInput.classList.remove('invalid');
-    }
-  });
-  emailInput.addEventListener('blur', () => {
-    if (emailInput.value !== '' && validateEmail(emailInput.value)) {
-      emailInput.classList.remove('invalid');
-    }
-  });
-  phoneInput.addEventListener('blur', () => {
-    if (
-      !phone.toString().length < 9 ||
-      !phone.toString().length > 9 ||
-      !isNaN(phone)
-    ) {
-      phoneInput.classList.remove('invalid');
-    }
-  });
-  serviceTypeInput.addEventListener('blur', () => {
-    if (serviceTypeInput.value !== 'Vyberte...') {
-      serviceTypeInput.classList.remove('invalid');
-    }
-  });
-  messageInput.addEventListener('blur', () => {
-    if (messageInput.value !== '') {
-      messageInput.classList.remove('invalid');
-    }
-  });
-};
+nameInput.addEventListener('blur', () => {
+  if (nameInput.value !== '') {
+    nameInput.classList.remove('invalid');
+  }
+});
+emailInput.addEventListener('blur', () => {
+  if (emailInput.value !== '' && validateEmail(emailInput.value)) {
+    emailInput.classList.remove('invalid');
+  }
+});
+phoneInput.addEventListener('blur', () => {
+  if (phoneInput.toString().length === 9 && isNaN(phoneInput.value) === false) {
+    phoneInput.classList.remove('invalid');
+  }
+});
+serviceTypeInput.addEventListener('blur', () => {
+  if (serviceTypeInput.value !== 'Vyberte...') {
+    serviceTypeInput.classList.remove('invalid');
+  }
+});
+messageInput.addEventListener('blur', () => {
+  if (messageInput.value !== '') {
+    messageInput.classList.remove('invalid');
+  }
+});
 
 contactForm.addEventListener('submit', e => {
   e.preventDefault();
@@ -92,20 +90,20 @@ contactForm.addEventListener('submit', e => {
   const serviceType = formData.get('service-type');
   const message = formData.get('message').trim();
 
-  validateOnBlur();
-
   if (name === '') {
+    isValidName = false;
     nameInput.classList.add('invalid');
   } else {
-    isValidName = true;
     nameInput.classList.remove('invalid');
+    isValidName = true;
   }
 
   if (email === '' || !validateEmail(email)) {
+    isValidEmail = false;
     emailInput.classList.add('invalid');
   } else {
-    isValidEmail = true;
     emailInput.classList.remove('invalid');
+    isValidEmail = true;
   }
 
   if (
@@ -113,24 +111,27 @@ contactForm.addEventListener('submit', e => {
     phone.toString().length > 9 ||
     isNaN(phone)
   ) {
+    isValidPhone = false;
     phoneInput.classList.add('invalid');
   } else {
-    isValidPhone = true;
     phoneInput.classList.remove('invalid');
+    isValidPhone = true;
   }
 
   if (serviceType === 'Vyberte...') {
+    isValidServiceType = false;
     serviceTypeInput.classList.add('invalid');
   } else {
-    isValidServiceType = true;
     serviceTypeInput.classList.remove('invalid');
+    isValidServiceType = true;
   }
 
   if (message === '') {
+    isValidMessage = false;
     messageInput.classList.add('invalid');
   } else {
-    isValidMessage = true;
     messageInput.classList.remove('invalid');
+    isValidMessage = true;
   }
 
   if (
@@ -140,14 +141,21 @@ contactForm.addEventListener('submit', e => {
     isValidServiceType &&
     isValidMessage
   ) {
-    console.log(name, email, phone, serviceType, message);
     fetch('../contact.php', {
       method: 'POST',
       body: formData,
     }).then(response => {
+      const p = document.createElement('p');
+      p.className = 'final-state';
+      contactForm.append(p);
       if (response.ok) {
         contactForm.reset();
+        p.innerHTML =
+          'Děkuji za vyplnění formuláře &#128522;  Co nejdříve se vám ozvu.';
       } else {
+        p.classList.add('error');
+        p.innerHTML =
+          'Formulář se nepodařilo odeslat &#128533; Nejspíš máte něco špatně vyplněného, zkuste to znovu';
         throw new Error('Něco se pokazilo!');
       }
     });
