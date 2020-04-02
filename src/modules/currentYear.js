adac10f185d7f84bc246306fec6d32939d23d58e
Lukáš Rác <lukis4822@seznam.cz>
2020-04-02 16:15:28 +0200

    Starting from the bottom

--- src/modules/currentYear.js
+++
@@ -1,7 +0,0 @@
-export const getCurrentYear = () => {
-  const now = new Date();
-  const currentYear = now.getFullYear();
-
-  const copyrightYear = document.querySelector('.current-year');
-  copyrightYear.textContent = currentYear;
-};
